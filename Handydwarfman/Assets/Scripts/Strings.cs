aeab81ab8c45e64b430aa0c0c60c181a6901e04e
MikeMG <michalek.mg@gmail.com>
2020-02-01 13:48:37 +0100

    strings & ropes

---
+++ Handydwarfman/Assets/Scripts/Strings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strings : MonoBehaviour
+{
+    public int partsNumber;
+    Rigidbody[] parts;
+    public GameObject stringPartPrefab;
+
+    private void Start()
+    {
+        parts = new Rigidbody [partsNumber];
+        for (int i = 0; i < partsNumber; i++)
+        {
+            //rigidbody = Instantiate(stringPartPrefab).GetComponent<Rigidbody>();
+            parts[i] = Instantiate(stringPartPrefab).GetComponent<Rigidbody>();
+            if (i > 0)
+                parts[i - 1].gameObject.GetComponent<CharacterJoint>().connectedBody = parts[i];
+        }
+    }
+}
