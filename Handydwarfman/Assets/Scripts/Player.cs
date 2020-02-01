ea3865bab3572279ed4cf78289bec1481876a6e8
Purban <xpurbanx@wp.pl>
2020-02-01 04:27:40 +0100

    Added player model and collision detection

---
+++ Handydwarfman/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Player : MonoBehaviour
+{
+    // singleton player instance
+    public static Player instance;
+
+    VRTK_TransformFollow follow;
+
+    private GameObject activeCameraRig;
+
+    private void Start()
+    {
+        follow = GetComponent<VRTK_TransformFollow>();
+        follow.gameObjectToFollow = activeCameraRig;
+        //follow.gameObjectToFollow = VRTK_DeviceFinder.PlayAreaTransform().gameObject;
+        follow.Follow();
+    }
+
+    void Awake()
+    {
+        VRTK_SDKManager.instance.AddBehaviourToToggleOnLoadedSetupChange(this);
+    }
+    protected virtual void OnEnable()
+    {
+        activeCameraRig = GameObject.FindGameObjectWithTag("CameraRig");
+    }
+    protected virtual void OnDestroy()
+    {
+        VRTK_SDKManager.instance.RemoveBehaviourToToggleOnLoadedSetupChange(this);
+    }
+}
+
