5864edee172ae0b710edce58d4c58f027efccd9a
Purban <xpurbanx@wp.pl>
2020-02-01 22:22:21 +0100

    Added nail snapzones

---
+++ Handydwarfman/Assets/Scripts/Util/DestroySnappedObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class DestroySnappedObject : MonoBehaviour
+{
+    public VRTK_SnapDropZone dropZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropZone = GetComponent<VRTK_SnapDropZone>();
+    }
+
+    private void OnEnable()
+    {
+        dropZone.ObjectSnappedToDropZone += new SnapDropZoneEventHandler(DestroyNail);
+    }
+
+    private void OnDisable()
+    {
+        dropZone.ObjectSnappedToDropZone -= new SnapDropZoneEventHandler(DestroyNail);
+    }
+
+    // Update is called once per frame
+    private void DestroyNail(object sender, SnapDropZoneEventArgs e)
+    {
+        Destroy(dropZone.GetCurrentSnappedObject());
+    }
+}
