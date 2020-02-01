56073ea493c30cfd66f87e70abfb4750e2bb2946
MikeMG <michalek.mg@gmail.com>
2020-02-01 01:19:05 +0100

    Basics - tool system

---
+++ Handydwarfman/Assets/Scripts/Fixing.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fixing : MonoBehaviour
+{
+    bool isFixed;
+
+    public ToolType toolNeeded;
+
+    void Fix()
+    {
+        switch (toolNeeded)
+        {
+            case ToolType.Rag:
+                // sprawdzanie czy kontroler jest w ruchu przez x sekund
+                // jeśli tak - pokaż particle, zmniejszaj opacity syfu, jeśli nie - zapauzuj timer
+                // naprawione
+                break;
+
+            case ToolType.Screwdriver:
+                // sprawdzanie rotacji kontrolera i rotowanie śruby (gotowy komponent z VRTK)
+                // wraz ze zmianą stopniową rotacji śruba wsuwa się dalej
+                // naprawione
+                break;
+
+            case ToolType.Hammer:
+                // sprawdzamy czy w poprzedniej klatce miał odpowiednią prędkość
+                // naprawione
+                break;
+
+            case ToolType.Pliers:
+                // sprawdzenie czy przytrzymano przycisk trigger (na końcówce)
+                // sprawdzenie czy końcówka znajduje się w swoim snapping zonie (w kontakcie)
+                // kiedy puścimy końcówkę w snapping zonie blokuje się możliwość podniesienia końcówki
+                // naprawione
+                break;
+
+            case ToolType.Scissors:
+                // sprawdzenie czy naciśnięto trigger
+                // sprawdzenie czy ucięliśmy kapsułkę pierwszą
+                // naprawione
+                break;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Tool>().toolType == toolNeeded && toolNeeded != ToolType.Hammer)
+            Fix();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Tool>().toolType == toolNeeded && toolNeeded == ToolType.Hammer)
+            Fix();
+    }
+}
---
+++ Handydwarfman/Assets/Scripts/Tool.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ToolType
+{
+    /// TUTAJ OKREŚLAMY JAKIE NARZĘDZIE JEST POTRZEBNE DO USUNIĘCIA USTERKI ///
+    Rag,            // Szmata
+    Screwdriver,    // Śrubokręt
+    Hammer,         // Młotek
+    Pliers,         // Kombinerki
+    Scissors,       // Nożyczki
+}
+
+public class Tool : MonoBehaviour
+{
+    public ToolType toolType;
+}
