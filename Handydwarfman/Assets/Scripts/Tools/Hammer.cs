81dbc5df576312adfb09248fe57d9166e02eda52
Purban <xpurbanx@wp.pl>
2020-02-01 14:50:09 +0100

    Started cleaning up script, hammer fixed

---
+++ Handydwarfman/Assets/Scripts/Tools/Hammer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer : Tool
+{
+    public Collider hammerHead;
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log(other.gameObject.name);
+
+    //    if (other == other.GetComponent<Nail>().topCollider)
+    //    {
+    //        Debug.Log(other.gameObject.name);
+    //        if (!isFixed)
+    //            other.GetComponent<Nail>()Fix();
+    //    }
+    //}
+
+}
