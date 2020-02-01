30415e35fc61402a526c2737d83b74be34401321
Mikołaj Przybylski <clovers02123@gmail.com>
2020-02-01 12:25:22 +0100

    Rag and dirt

--- Handydwarfman/Assets/Scripts/Fixing.cs
+++ Handydwarfman/Assets/Scripts/Fixing.cs
@@ -5,18 +5,35 @@ using UnityEngine;
 
 public class Fixing : MonoBehaviour
 {
-    bool isFixed;
+    public ParticleSystem dirtParticle;
+    public ToolType toolNeeded; // Narzędzie potrzebne do naprawienia tej usterki
+    public float positionChangeNeeded;  // O ile trzeba przesunąć gwóźdź
 
-    public ToolType toolNeeded;
+    bool isFixed;   // Czy usterka jest naprawiona
+    float decreasingDirtOverTime = 0.01f;   // O ile zmniejsza się alpha materiału brudu, każdorazowo gdy prędkość ścierki jest > 0.3f
+    float decreasingPosition = 0.03f;   // O ile gwóźdź 
 
     void Fix(GameObject usedTool)
     {
         switch (toolNeeded)
         {
             case ToolType.Rag:
-                // sprawdzanie czy kontroler jest w ruchu przez x sekund
-                // jeśli tak - pokaż particle, zmniejszaj opacity syfu, jeśli nie - zapauzuj timer
-                // naprawione
+                if (usedTool.GetComponent<Rigidbody>().velocity.magnitude > 0.3f)
+                {
+                    if (!dirtParticle.isPlaying) dirtParticle.Play();
+                    Color color = GetComponent<MeshRenderer>().material.color;
+                    GetComponent<MeshRenderer>().material.color = new Color(color.r, color.g, color.b, color.a - decreasingDirtOverTime);
+                    if (color.a - decreasingDirtOverTime <= 0)
+                    {
+                        isFixed = true;
+                    }
+                }
+
+                else
+                {
+                    dirtParticle.Stop();
+                }
+
                 break;
 
             case ToolType.Screwdriver:
@@ -27,7 +44,11 @@ public class Fixing : MonoBehaviour
 
             case ToolType.Hammer:
                 if (usedTool.GetComponent<Rigidbody>().velocity.magnitude > 1f)
-                    transform.position = new Vector3(transform.position.x, transform.position.y - 0.03f, transform.position.z);
+                {
+                    positionChangeNeeded -= decreasingPosition;
+                    transform.position = new Vector3(transform.position.x, transform.position.y - decreasingPosition, transform.position.z);
+                    if (positionChangeNeeded <= 0) isFixed = true;
+                }
                 break;
 
             case ToolType.Pliers:
@@ -60,5 +81,11 @@ public class Fixing : MonoBehaviour
             if (other.gameObject.GetComponent<Tool>().toolType == toolNeeded && toolNeeded == ToolType.Hammer && !isFixed)
                 Fix(other.gameObject);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<Tool>() != null && dirtParticle != null)
+            if (dirtParticle.isPlaying) dirtParticle.Stop();
     }
 }
