a71923ad7efffb4d72343a4b55c6612feedc7e80
Mikołaj Przybylski <clovers02123@gmail.com>
2020-02-02 01:26:25 +0100

    Doll

---
+++ Handydwarfman/Assets/Scripts/Defects/CableEnding.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CableEnding : MonoBehaviour
+{
+    public GameObject holdingHand;
+
+    public void OnGrab(GameObject hand)
+    {
+        holdingHand = hand;
+    }
+
+    public void OnUngrab(GameObject hand)
+    {
+        if (holdingHand == hand)
+        {
+            holdingHand = null;
+        }
+    }
+}
---
+++ Handydwarfman/Assets/Scripts/Defects/CableEntry.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class CableEntry : MonoBehaviour
+{
+    public GameObject positionForCable;
+    public GameObject left, right;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CableEnding")
+        {
+            other.transform.position = positionForCable.transform.position;
+            other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            other.GetComponent<CableEnding>().holdingHand.GetComponent<VRTK_InteractGrab>().ForceRelease();
+        }
+    }
+}
